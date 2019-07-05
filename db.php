4f7ec6e4eb880fb5cf255d1be6d06816e03b42d1
sravanadepu <32012809+sravanadepu@users.noreply.github.com>
2019-07-05 19:52:54 +0530

    Update db.php

--- db.php
+++ db.php
@@ -1,7 +1,7 @@
 <?php
-$host = "localhost";
-$user = "root";
-$pass = "";
-$db_name = "credit_management";
+$host = "sql112.epizy.com";
+$user = "epiz_24147107";
+$pass = "Op2aBxgXF27Es";
+$db_name = "epiz_24147107_sai";
 $con = new mysqli( $host, $user, $pass, $db_name );
 ?>
